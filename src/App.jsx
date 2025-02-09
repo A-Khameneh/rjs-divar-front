1177c9abeeb4cabb9d039cd3ede89cf971eefe1c
Davoud A.Khameneh <mt.d.abdollahian@gmail.com>
2025-02-09 23:55:21 +0330

    install react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import { BrowserRouter } from "react-router-dom";
 import Router from "./router/Router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import defaultOptions from "./configs/reactQueryConfigs";
 
 function App() {
+
+  const queryClient = new QueryClient( { defaultOptions } );
+
   return (
 
-    <BrowserRouter>
+    <QueryClientProvider client={ queryClient } >
+
+      <BrowserRouter>
     
-      <Router />
+        <Router />
     
-    </BrowserRouter>
+      </BrowserRouter>
+
+      <ReactQueryDevtools />
+
+    </QueryClientProvider>
 
   )
 }
