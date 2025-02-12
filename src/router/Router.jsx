20cc1fb51c58d75531d72af65a6a54975b33de27
Davoud A.Khameneh <mt.d.abdollahian@gmail.com>
2025-02-12 09:05:32 +0330

    get access token value

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,12 +1,18 @@
 import { Route, Routes } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 
 import AuthPage from "pages/AuthPage";
 import HomePage from "pages/HomePage";
 import DashboardPage from "pages/DashboardPage";
 import AdminPage from "pages/AdminPage";
 import PageNotFound from "pages/404";
+import { getProfile } from "services/user";
 
 export default function Router() {
+
+    const { data, isLoading, error } = useQuery( ["profile"], getProfile );
+
+    console.log({ data, isLoading, error });
 
     return <Routes>
 
--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -1,8 +1,28 @@
 const setCookie = tokens => {
 
-    document.cookie = ` accessToken = ${ tokens.accessToken }; SameSite=Lax; max-age = ${ 1 * 24 * 60 * 60 } `;
-    document.cookie = ` refreshToken = ${ tokens.refreshToken }; SameSite=Lax; max-age = ${ 30 * 24 * 60 * 60 } `;
+    document.cookie = ` accessToken = ${ tokens.accessToken }; SameSite=Lax; max-age = ${ 30 * 24 * 60 * 60 } `;
+    document.cookie = ` refreshToken = ${ tokens.refreshToken }; SameSite=Lax; max-age = ${ 90 * 24 * 60 * 60 } `;
 
 }
 
-export { setCookie };
+// const setCookie = tokens => {
+
+//     const setCookieValue = (name, value, days) => {
+//       const date = new Date();
+//       date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+//       const expires = "expires=" + date.toUTCString();
+//       document.cookie = `${name}=${value}; ${expires}; SameSite=Lax; path=/`;
+//     };
+  
+//     setCookieValue("accessToken", tokens.accessToken, 1);
+//     setCookieValue("refreshToken", tokens.refreshToken, 30);
+
+// };
+
+const getCookie = cookieName => {
+
+    return document.cookie.split(";").find( token => token.trim().split("=")[0] === cookieName )?.split("=")[1];
+
+}
+
+export { setCookie, getCookie };
