44b6359f1be92d56498e8a4af94a2fd285e5cc2d
Davoud A.Khameneh <mt.d.abdollahian@gmail.com>
2025-02-26 18:18:52 +0330

    fetching categories for home page

--- src/components/templates/SideBar.jsx
+++ src/components/templates/SideBar.jsx
@@ -1,10 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
+import { getCategory } from "src/services/admin";
 
 
 export default function SideBar() {
 
+    const { data } = useQuery( ["get-categories"], getCategory );
+    console.log(data);
+
     return <div>
 
-        SideBar
+        <h4> دسته ها </h4>
+
+        <ul>
+
+            { data?.data?.map( category => (
+
+                <li key={ category._id } >
+
+                    <img src={`${ category?.icon }.svg`} />
+                    <p> { category.name } </p>
+
+                </li>
+
+            ) ) }
+
+        </ul>
 
     </div>
 
