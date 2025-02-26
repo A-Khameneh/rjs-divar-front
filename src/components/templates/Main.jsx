6fab6d6812d6615663f73187402fe49af324e96f
Davoud A.Khameneh <mt.d.abdollahian@gmail.com>
2025-02-26 19:58:37 +0330

    Main editing

--- src/components/templates/Main.jsx
+++ src/components/templates/Main.jsx
@@ -1,10 +1,37 @@
+import { sp } from "src/utils/numbers";
 
 
-export default function Main() {
+export default function Main({ posts }) {
+
+    const baseURL = import.meta.env.VITE_BASE_URL;
 
     return <div>
 
-        Main
+        {
+
+            posts?.data?.posts?.map( post => (
+
+                <div key={ post._id } >
+
+                    <div>
+
+                        <p> { post?.options?.title } </p>
+                        <div>
+
+                            <p> { sp( post?.amount ) } تومان </p>
+                            <span> { post?.options?.city } </span>
+
+                        </div>
+
+                    </div>
+
+                    <img src={ `${ baseURL }${ post.images[0] }` } />
+
+                </div>
+
+            ) )
+
+        }
 
     </div>
 
